2e15d0721ac97c955105c7c5079735845743ad04
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-01-24 12:21:38 +0330

    Add CNNPackages

---
+++ CNNHardware/CNNHardware.srcs/sources_1/new/CNNPackages.vhd
@@ -0,0 +1,37 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/24/2023 11:47:07 AM
+-- Design Name: 
+-- Module Name: CNNPackages - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+
+package CNNPackages is
+    subtype pixle is integer range 0 to 255; 
+    type win_t is array (0 to 2, 0 to 2) of pixle;
+    type filter_t is array (0 to 2, 0 to 2) of real;
+	type win_array_t is array (0 to 125, 0 to 125) of win_t; -- windows taken from input pic
+	type win_buffer_t is array (integer range <>, 0 to 125, 0 to 125) of win_t; -- windows in the queue
+	type filter_array_t is array(integer range <>) of filter_t;
+	type pic_t is array (0 to 127, 0 to 127) of pixle;
+	function get_buffer_windows (buffer_index : integer) return win_array_t ;
+	function generate_random_filter return filter_t;
+	function get_all_windows_from_file return win_array_t;
+end package;
+
+-- TODO: Write function bodies
