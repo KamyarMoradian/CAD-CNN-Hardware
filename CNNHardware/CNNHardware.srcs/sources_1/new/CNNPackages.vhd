8b904154ed99c7b32aac3c86fec02192b8674462
Kamyar Moradian <74311911+KamyarMoradian@users.noreply.github.com>
2023-01-24 15:48:40 +0330

    Update CNNPackages.vhd
    add type win_array_buffer_t and changed type win_buffer_t. Also change the return type of get_buffer_windows to win_buffer_t

--- CNNHardware/CNNHardware.srcs/sources_1/new/CNNPackages.vhd
+++ CNNHardware/CNNHardware.srcs/sources_1/new/CNNPackages.vhd
@@ -26,10 +26,11 @@ package CNNPackages is
     type win_t is array (0 to 2, 0 to 2) of pixle;
     type filter_t is array (0 to 2, 0 to 2) of real;
 	type win_array_t is array (0 to 125, 0 to 125) of win_t; -- windows taken from input pic
-	type win_buffer_t is array (integer range <>, 0 to 15875) of win_t; -- windows in the queue
+	type win_buffer_t is array (0 to 15875) of win_t; -- windows in the each buffer 
+	type win_array_buffer_t is array (integer range <>) of win_buffer_t; -- all the buffers of windows
 	type filter_array_t is array(integer range <>) of filter_t;
 	type pic_t is array (0 to 127, 0 to 127) of pixle;
-	function get_buffer_windows (buffer_index : integer) return win_array_t ;
+	function get_buffer_windows (buffer_index : integer) return win_buffer_t ;
 	function generate_random_filter return filter_t;
 	function get_all_windows_from_file return win_array_t;
 end package;
